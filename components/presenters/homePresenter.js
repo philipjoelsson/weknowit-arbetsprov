da602e675c3704747e80ca8dc5682745429a83c4
pjoe <pjoe@kth.se>
2022-04-06 09:26:18 +0200

    Created folders for presenters and screen, and also created home page

---
+++ components/presenters/homePresenter.js
@@ -0,0 +1,8 @@
+import HomeScreen from './../screens/homeScreen.js';
+
+const HomePresenter = () => {
+
+  return <HomeScreen />
+}
+
+export default HomePresenter;
---
+++ components/screens/homeScreen.js
@@ -0,0 +1,50 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+
+  return (
+
+    <View style={styles.container}>
+      <Text style={styles.title}> CityPop </Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}> SEARCH BY CITY </Text>
+      </Pressable>
+      <Pressable style={styles.button}>
+        <Text style={styles.text}> SEARCH BY COUNTRY </Text>
+      </Pressable>
+    </View>
+
+  )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    position: 'absolute',
+    top: 200,
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+    width: 250,
+    height: 50,
+    margin: 5,
+  },
+  text: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+})
